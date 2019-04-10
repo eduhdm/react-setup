3ea3a9a229bdb68e5ac083f4b5fe341b3fb46615
ëduhdm0105 <duhdm0105@gmail.com>
2019-04-09 23:38:35 -0300

    ENH: Use material ui theme provider

---
+++ src/common/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+/**
+ * TODO:
+ *  - Create app palette
+ *  - Override components reused througout the app
+ *  - Customize app breakpoints
+ */
+const theme = createMuiTheme();
+
+export default theme;
+
