2b3ac145cfef813d665a08dc2d9a87385ade6c71
ëduhdm0105 <duhdm0105@gmail.com>
2019-04-09 23:44:26 -0300

    ENH: Use own JSS provider

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import {
+  createGenerateClassName,
+  jssPreset,
+  MuiThemeProvider,
+} from '@material-ui/core/styles';
+
 import theme from 'common/theme';
 
+const generateClassName = createGenerateClassName();
+const jss = create(jssPreset());
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <div className="App">
-          Hello mundo!
-        </div>
-      </MuiThemeProvider>
+      <JssProvider jss={jss} generateClassName={generateClassName}>
+        <MuiThemeProvider theme={theme}>
+          <div className="App">
+            Hello mundo!
+          </div>
+        </MuiThemeProvider>
+      </JssProvider>
     );
   }
 }
