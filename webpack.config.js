65ac354fd749a9a1fe336293e9c67ae3d193bbc2
ëduhdm0105 <duhdm0105@gmail.com>
2019-04-09 23:06:28 -0300

    ENH: Change index file to use pug and load it using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,56 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+            loader: "babel-loader"
+            }
+        },
+        {
+            test: /\.html$/,
+            use: [
+            {
+                loader: "html-loader"
+            }
+            ]
+        },
+        {
+            test: /\.pug$/,
+            use: [{
+              loader: 'pug-loader',
+            }],
+        },
+        {
+            test: /\.(png|jpg|gif|woff2|woff|ttf|eot|pdf)$/,
+            use: [
+            'image-webpack-loader',
+            {
+                loader: 'file-loader',
+                options: {
+                outputPath: 'assets/',
+                },
+            }],
+        },
         ]
-      }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.pug",
+        }),
+        new CopyPlugin([
+            { from: './manifest.json', to: './manifest.json' },
+            { from: './src/common/assets/images/favicon.ico', to: './favicon.ico' },
+        ]),
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
 };
